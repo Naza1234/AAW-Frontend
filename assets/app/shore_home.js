d5cb8ca2d8635a5392cf4135a742862b18335a4c
Ozurmba chibuike <ocbmf17@gmail.com>
2023-12-19 02:31:48 +0100

    added store
    added the frontend for the store section

---
+++ assets/app/shore_home.js
@@ -0,0 +1,14 @@
+
+var buttons =document.querySelectorAll(".quick_nav span")
+
+
+    buttons[0].addEventListener("click",()=>{
+     document.getElementsByClassName("quick_nav")[0].classList.add("active")
+     document.getElementsByClassName("quick_nav")[0].classList.remove("explore")
+    })
+    buttons[1].addEventListener("click",()=>{
+        document.getElementsByClassName("quick_nav")[0].classList.remove("active")
+     document.getElementsByClassName("quick_nav")[0].classList.add("explore")
+    })
+
+
